4369719b1647e5278332ae0dd494a3be2bd88061
David Fong <dav770@outlook.com>
2020-01-28 16:46:36 -0500

    P1: Initial commit

---
+++ project/project/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/********************************************************************
+
+Project 1: Happiness is Sisyphus
+Author Name: David Fong
+
+This is a project that is about making a program
+that is related to Sisyphus which is about a guy who is pushing
+a boulder in a hill which the boulder keeps fall back down.
+So the task is never ending which I have to create a project that
+reflects the story of Sisyphus.
+
+*********************************************************************/
